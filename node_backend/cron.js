4d5a1da3cbcc18ce1d33cddf4e3aebeabbb8cf6e
Tejas Kothari <tejas23682320@gmail.com>
2022-05-29 22:27:23 +0800

    Cron job script in Node backend

--- node_backend/cron.js
+++ node_backend/cron.js
@@ -1,23 +1,52 @@
 import { loadStdlib } from "@reach-sh/stdlib";
 import dotenv from "dotenv";
 import * as backend from "../reach_sc/build/index.main.mjs";
+import { promises as fs } from "fs";
 
 const script = async () => {
   const reach = loadStdlib("ALGO");
-  reach.setProviderByEnv({
-    ALGO_TOKEN:
-      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-    ALGO_SERVER: "http://localhost",
-    ALGO_PORT: 4001,
-    ALGO_INDEXER_TOKEN:
-      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-    ALGO_INDEXER_SERVER: "http://localhost",
-    ALGO_INDEXER_PORT: 8980,
+
+  const appID = await fs.readFile(process.env.appID_filename, {
+    encoding: "utf8",
   });
 
   const backendAcc = await reach.newAccountFromMnemonic(process.env.mnemonic);
+  const ctc = backendAcc.contract(backend, appID);
 
-  const ctc = backendAcc.contract(backend, process.env.appID);
+  const networkSecsPromise = reach.getNetworkSecs();
+  const deadlineSecsPromise = ctc.unsafeViews.UserView.deadlineSecs();
+
+  const [networkSecs, deadlineSecs] = (
+    await Promise.all([networkSecsPromise, deadlineSecsPromise])
+  ).map((res) => res.toNumber());
+
+  console.log("Network time: " + networkSecs);
+  console.log("Lottery deadline: " + deadlineSecs);
+
+  if (networkSecs < deadlineSecs) {
+    const testAcc = await reach.newTestAccount(1000000);
+    const ctc_test = testAcc.contract(backend, appID);
+
+    await testAcc.tokenAccept(237);
+    await reach.transfer(backendAcc, testAcc, 200, 237);
+
+    await ctc_test.apis.UserAPI.deposit(reach.bigNumberify(5));
+  } else {
+    const requestingRN = await ctc.unsafeViews.UserView.requestingRN();
+
+    if (requestingRN) {
+      const runningProb = (
+        await ctc.unsafeViews.UserView.runningProb()
+      ).toNumber();
+      console.log("Probability array size = " + runningProb);
+
+      const rn = reach.bigNumberify(
+        Math.floor(Math.random() * (runningProb + 1))
+      );
+      await ctc.apis.UserAPI.supplyRN(rn);
+      console.log("Supplying random number " + rn.toNumber());
+    }
+  }
 };
 
 dotenv.config();
--- node_backend/deploy.js
+++ node_backend/deploy.js
@@ -1,53 +1,45 @@
 import { loadStdlib } from "@reach-sh/stdlib";
 import dotenv from "dotenv";
 import * as backend from "../reach_sc/build/index.main.mjs";
+import { promises as fs } from "fs";
 
 const script = async () => {
   const reach = loadStdlib("ALGO");
-  reach.setProviderByEnv({
-    ALGO_TOKEN:
-      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-    ALGO_SERVER: "http://localhost",
-    ALGO_PORT: 4001,
-    ALGO_INDEXER_TOKEN:
-      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-    ALGO_INDEXER_SERVER: "http://localhost",
-    ALGO_INDEXER_PORT: 8980,
-  });
 
   const backendAcc = await reach.newAccountFromMnemonic(process.env.mnemonic);
-  const fundingAcc = await reach.newAccountFromMnemonic(
-    "cost tell employ ship clog number boss rare vessel elite before vanish deer taxi because month wise own stock walk cruise link jelly about mistake"
-  );
-  await reach.transfer(fundingAcc, backendAcc, 10000000);
-  const gil = await reach.launchToken(fundingAcc, "GIL COIN", "GIL");
-  console.log("New token ID: " + gil.id.toNumber());
-  await backendAcc.tokenAccept(gil.id);
-  await gil.mint(backendAcc, 20000);
+
+  let depositToken;
+  const vaultAmt = 2300;
+  const relativeDeadlineSecs = 120;
+
+  if (process.env.network === "devnet") {
+    if (await reach.canFundFromFaucet()) {
+      await reach.fundFromFaucet(backendAcc, 10000000);
+    }
+
+    const gil = await reach.launchToken(backendAcc, "GIL COIN", "GIL");
+    console.log("New token ID: " + gil.id.toNumber());
+    await backendAcc.tokenAccept(gil.id);
+    await gil.mint(backendAcc, 20000);
+    depositToken = gil.id;
+  }
 
   const ctc = backendAcc.contract(backend);
 
-  let salt;
-  const random = () => {
-    salt = reach.randomUInt();
-    return salt;
-  };
-  const initRN = reach.randomUInt();
-  const depositToken = gil.id;
-  const vaultAmt = 2300;
-  const relativeDeadlineSecs = 180;
+  await reach.withDisconnect(() =>
+    ctc.participants.PoolCreator({
+      log: (...args) => console.log(...args),
+      disconnect: () => reach.disconnect(null),
+      depositToken: depositToken,
+      vaultAmt: vaultAmt,
+      relativeDeadlineSecs: relativeDeadlineSecs,
+    })
+  );
 
-  ctc.participants.PoolCreator({
-    log: (...args) => console.log(...args),
-    random: random,
-    initRN: initRN,
-    depositToken: depositToken,
-    vaultAmt: vaultAmt,
-    relativeDeadlineSecs: relativeDeadlineSecs,
-  });
+  const appID = (await ctc.getInfo()).toNumber();
+  console.log(`The contract is deployed as = ${appID}`);
 
-  const appID = await ctc.getInfo();
-  console.log(`The contract is deployed as = ${JSON.stringify(appID)}`);
+  await fs.writeFile(process.env.appID_filename, appID.toString());
 };
 
 dotenv.config();
